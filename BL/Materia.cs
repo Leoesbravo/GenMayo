db6ac059284f5bb8e1052f7ef41820e71e896346
AngelMendoza1 <89880077+AngelMendoza1@users.noreply.github.com>
2022-05-25 18:17:44 -0500

    Add

--- BL/Materia.cs
+++ BL/Materia.cs
@@ -1,13 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace BL
 {
     public class Materia
     {
+        public static ML.Result Add(string nombre, decimal costo, byte creditos)
+        {
+            //instancia
+            ML.Result result = new ML.Result();
 
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString()))
+                {
+                    string query = "INSERT INTO [Materia]  ([Nombre] ,[Costo] ,[Creditos]) VALUES (@Nombre, @Costo, @Creditos )";
+
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+
+                        SqlParameter[] parameter = new SqlParameter[3];
+
+                        parameter[0] = new SqlParameter("@Nombre", SqlDbType.VarChar);
+                        parameter[0].Value = nombre;
+
+                        parameter[1] = new SqlParameter("@Costo", SqlDbType.Decimal);
+                        parameter[1].Value = costo;
+
+                        parameter[2] = new SqlParameter("@Creditos", SqlDbType.TinyInt);
+                        parameter[2].Value = creditos;
+
+                        cmd.Parameters.AddRange(parameter);
+                        cmd.Connection.Open();
+
+                        int RowsAffected = cmd.ExecuteNonQuery();
+
+                        if (RowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                            
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
     }
 }
---
+++ PL/Materia.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Materia
+    {
+        public static void Add()
+        {
+
+            Console.WriteLine("Ingrese el nombre de la materia");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el costo de la materia");
+            decimal costo = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese los creditos de la materia");
+            byte creditos = byte.Parse(Console.ReadLine());
+
+            //instancia
+            // ML.Result result = new ML.Result();
+
+            ML.Result result = BL.Materia.Add(nombre, costo, creditos);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("La materia se ha agregado");
+            }
+            else
+            {
+                Console.WriteLine("La materia no se pudo agregar " + result.ErrorMessage);
+            }
+        }
+    }
+}
